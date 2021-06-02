612186d48037e3167aa5c4e1b345fecfa6d16b3c
Suleman Imdad <31928839+perceptionfoundry@users.noreply.github.com>
2021-06-02 13:56:08 +0500

    Welcome screen fixing

--- Nommies/Nommies/App/WelcomeView.swift
+++ Nommies/Nommies/App/WelcomeView.swift
@@ -17,71 +17,87 @@ struct WelcomeView: View {
 //        Text("Hello, PROD!")
 //            .padding()
 //        #endif
-        ScrollView{
+        ScrollView(.vertical, showsIndicators: false){
         VStack{
             
             Image("Logo_small")
             
             Image("People")
+                .resizable()
+                .frame( height: GetRect().height * 0.325, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
             
             VStack{
                 
-                VStack(alignment:.leading, spacing:10) {
-                    HStack{
-                        Spacer()
-                    }
-                    Text("Welcome Back!")
-                        .font(.custom("HelveticaNeue-Bold", size: 30))
+                ZStack(alignment:.top) {
+                    RoundedRectangle(cornerRadius: 30)
+                                    .fill(Color.white)
+                        .frame(width: GetRect().width, height: GetRect().height * 0.675, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .shadow(radius: 1)
+                        .offset(x: 0, y: -20.0)
+                                    
+                    
+                    VStack(alignment:.leading, spacing:10) {
+                      
+                        Text("Welcome Back!")
+                            .font(.custom("HelveticaNeue-Bold", size: 30))
+                            
+                        Text("it’s good to see you again, lets start!")
+                            .font(.custom("HelveticaNeue-Regular", size: 15))
+                            .foregroundColor(.gray)
+                            .padding(.bottom)
                         
-                    Text("it’s good to see you again, lets start!")
-                        .font(.custom("HelveticaNeue-Regular", size: 15))
-                        .foregroundColor(.gray)
-                        .padding(.bottom)
-                    
-                    Text("Email")
-                        .font(.custom("HelveticaNeue-Regular", size: 14))
-                    
-                        //MARK: Email Textfield
-                    TextField("eg. username@email.com", text: .constant(""))
-                        .font(.custom("HelveticaNeue-Regular", size: 14))
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 18)
-                                .stroke(Color.gray)
-                                )
-                    
-                    SubmissionButtonView(action: signInAction, title: "Sign IN")
-                }.padding()
-                
-            }.background(RoundedRectangle(cornerRadius: 30)
-                            .fill(Color.white)
-                            .frame(width: GetRect().width, height: GetRect().height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            .shadow(radius: 1)
-                            .offset(x: 0, y: UIScreen.main.bounds.height *  0.3 ))
-            
-            Spacer()
-            
-            //MARK: Term Button
-            VStack{
-            Text("By signing in, you agree with our") + Text(" Term of Use")
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            }.padding(.bottom, 2)
-            .font(.custom("HelveticaNeue-Regular", size: 15))
-            .onTapGesture {
-                
-            }
-            
-            //MARK: Privacy Button
-            VStack{
-            Text(" and") +
-                Text(" Privacy Policy ")
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            }
-            .font(.custom("HelveticaNeue-Regular", size: 15))
-            .onTapGesture {
+                        Text("Email")
+                            .font(.custom("HelveticaNeue-Regular", size: 14))
+                        
+                            //MARK: Email Textfield
+                        TextField("eg. username@email.com", text: .constant(""))
+                            .font(.custom("HelveticaNeue-Regular", size: 14))
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(Color.gray)
+                                    )
+                        
+                        SubmissionButtonView(action: signInAction, title: "Sign IN")
+                            .padding(.bottom, 30)
+                       
+                        HStack{
+                            Spacer()
+                        VStack{
+                            //MARK: Term Button
+                            VStack{
+                            Text("By signing in, you agree with our") + Text(" Term of Use")
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            }.padding(.bottom, 2)
+                            .font(.custom("HelveticaNeue-Regular", size: 15))
+                            .onTapGesture {
+                                
+                            }
+                            
+                            //MARK: Privacy Button
+                            VStack{
+                            Text(" and") +
+                                Text(" Privacy Policy ")
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            }
+                            .font(.custom("HelveticaNeue-Regular", size: 15))
+                            .onTapGesture {
+                                
+                            }
+                        }
+                            Spacer()
+                    }
+                    }.padding()
+                }
                 
             }
-            .padding(.bottom)
+           
+            
+//            Spacer()
+            
+           
+           
         }// VStack - MAIN (END)
         }// ScrollView - MAIN (END)
             
